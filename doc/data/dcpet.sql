ff71c31135e8015f1a5203ace7531faf2b5539e7
hnsylitao <hnsylitao1@gmail.com>
2016-03-21 17:45:55 +0800

    数据库 及 七牛信息

---
+++ doc/data/dcpet.sql
@@ -0,0 +1,98 @@
+/*==============================================================*/
+/* DBMS name:      MySQL 5.0                                    */
+/* Created on:     2016/3/21 13:48:14                           */
+/*==============================================================*/
+
+
+drop table if exists Dc_Area;
+
+drop table if exists Dc_Pet;
+
+drop table if exists Dc_PetType;
+
+drop table if exists Dc_Pet_Image;
+
+drop table if exists Dc_User;
+
+drop table if exists Dc_UserData;
+
+/*==============================================================*/
+/* Table: Dc_Area                                               */
+/*==============================================================*/
+create table Dc_Area
+(
+   id                   int not null auto_increment,
+   no                   varchar(32) not null,
+   name                 varchar(500) not null,
+   primary key (id)
+);
+
+/*==============================================================*/
+/* Table: Dc_Pet                                                */
+/*==============================================================*/
+create table Dc_Pet
+(
+   id                   int not null auto_increment,
+   pettype              int not null,
+   name                 varchar(102) not null,
+   sex                  int not null,
+   age                  int not null,
+   area                 varchar(32) not null,
+   address              varchar(500),
+   firsttime            datetime not null,
+   lasttime             datetime,
+   token                char(32) not null,
+   demand               varchar(2000),
+   disable              bit(1) not null,
+   primary key (id)
+);
+
+/*==============================================================*/
+/* Table: Dc_PetType                                            */
+/*==============================================================*/
+create table Dc_PetType
+(
+   id                   int not null auto_increment,
+   pettype              varchar(500) not null,
+   primary key (id)
+);
+
+/*==============================================================*/
+/* Table: Dc_Pet_Image                                          */
+/*==============================================================*/
+create table Dc_Pet_Image
+(
+   id                   int not null auto_increment,
+   petid                int not null,
+   image                varchar(500) not null,
+   firsttime            datetime not null,
+   primary key (id)
+);
+
+/*==============================================================*/
+/* Table: Dc_User                                               */
+/*==============================================================*/
+create table Dc_User
+(
+   token                char(36) not null,
+   uuid                 varchar(102) not null,
+   device               varchar(102) not null,
+   platform             varchar(102) not null,
+   firstip              varchar(102) not null,
+   lastip               varchar(102),
+   firsttime            datetime not null,
+   lasttime             datetime,
+   disable              bit(1) not null,
+   primary key (token)
+);
+
+/*==============================================================*/
+/* Table: Dc_UserData                                           */
+/*==============================================================*/
+create table Dc_UserData
+(
+   token                char(36) not null,
+   phonenumber          varchar(102) not null,
+   name                 varchar(102) not null
+);
+
---
+++ doc/qiniu/qiniu.md
@@ -0,0 +1,7 @@
+# 七牛平台
+    - ACCESS_KEY=aG3Ou2YzCPu0D_g1SPVKW911NfB3Q9RbCBq3TTso
+    - SECRET_KEY=e6FWFz2O2ndZ02IHl9Sh1xAl85t-CUSe26zam218
+    - CDNURL=http://7xs3ku.com1.z0.glb.clouddn.com/
+    - ROOM=petsdcimages
+	- 账号=wang130@qq.com
+	- 密码=w19897361
